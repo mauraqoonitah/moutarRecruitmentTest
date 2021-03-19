9432a809cbb44389c44c536d25a299aff7a7a96d
mauraqp <mauraqoonitah@gmail.com>
2021-03-20 00:27:24 +0700

    plain css & navbar update

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+// burger show
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+//open
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+})
+
+//close
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
+
+//remove menu link
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//scroll sections active link
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+
+        }
+
+
+    })
+}
